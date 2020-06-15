f6346fd613cbb27a93750012d9d878a63e5b33e0
Tomas <talvarez2@uc.cl>
2020-06-15 03:37:03 -0400

    Updates

---
+++ precache-manifest.c92d746e6e2e76b5271e24da3c4fa70a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d7c5b8a2e0bb316b5b325c45f5bc63",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "8eb81a34a44f704a5baa",
+    "url": "/React-Todo-App/static/js/2.cea1410b.chunk.js"
+  },
+  {
+    "revision": "0bca0e4b5ec7498b8f9e",
+    "url": "/React-Todo-App/static/js/main.9134a329.chunk.js"
+  },
+  {
+    "revision": "3328124a2c5a54bfc0fc",
+    "url": "/React-Todo-App/static/js/runtime-main.be2f18b1.js"
+  }
+]);
